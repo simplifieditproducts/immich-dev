a5827143a401d8913f79720658e5b3ffe6db2c29
Hao Xi <haoxi911@gmail.com>
2025-07-15 18:10:42 -0400

    feat: test patching the logo in the `@immich/ui` package.

--- web/patch-package.cjs
+++ web/patch-package.cjs
@@ -3,10 +3,7 @@ const path = require("path");
 
 // This script patches the immich-ui logo in the web project.
 const oldLogoPath = path.resolve(__dirname, "./node_modules/@immich/ui/dist/assets/immich-logo.svg");
-let newLogoPath = path.resolve(__dirname, "./web/static/logo.svg");
-if (!fs.existsSync(newLogoPath)) {
-  newLogoPath = path.resolve(__dirname, "./static/logo.svg");
-}
+let newLogoPath = path.resolve(__dirname, "./static/logo.svg");
 if (!fs.existsSync(newLogoPath)) {
   console.error("❌ immich-ui's new logo was not found at", newLogoPath);
   process.exit(1);
