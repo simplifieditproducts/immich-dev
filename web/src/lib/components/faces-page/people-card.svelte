8e8177991ba8af5a41e5aa606ba3a953af0ab35d
Hao Xi <haoxi911@gmail.com>
2025-08-29 15:20:34 -0400

    feat: adjust the name input field in the '/people' page.

--- web/src/lib/components/faces-page/people-card.svelte
+++ web/src/lib/components/faces-page/people-card.svelte
@@ -13,6 +13,7 @@
     mdiHeart,
     mdiHeartMinusOutline,
     mdiHeartOutline,
+    mdiPencilPlusOutline,
   } from '@mdi/js';
   import { t } from 'svelte-i18n';
   import ImageThumbnail from '../assets/thumbnail/image-thumbnail.svelte';
@@ -30,6 +31,7 @@
   }
 
   let { person, onSetBirthDate, onMergePeople, onHidePerson, onToggleFavorite, onInputFocusIn, onInputFocusOut, onInput }: Props = $props();
+  let currentInputValue = $state(person.name);
 </script>
 
 <div
@@ -58,18 +60,21 @@
     </div>
   </a>
 
-  <div>
+  <div class="relative">
     <input
       type="text"
-      class="bg-white border-b dark:bg-immich-dark-gray border-gray-300 placeholder-gray-400 text-center dark:border-gray-900 w-full  mt-3 py-1.5 font-medium px-3 text-sm text-immich-primary dark:text-immich-dark-primary focus:border-immich-primary focus:dark:border-immich-dark-primary"
-      value={person.name}
-      placeholder="Who is this?"
+      class="bg-white border-b dark:bg-immich-dark-gray border-gray-300 placeholder-gray-400 text-center dark:border-gray-900 w-full mt-3 py-1.5 font-medium px-3 text-sm text-immich-primary dark:text-immich-dark-primary focus:border-immich-primary focus:dark:border-immich-dark-primary"
+      bind:value={currentInputValue}
+      placeholder={$t('add_a_name')}
       use:shortcut={{ shortcut: { key: 'Enter' }, onShortcut: (e) => e.currentTarget.blur() }}
       onfocusin={() => onInputFocusIn()}
       onfocusout={() => onInputFocusOut()}
       oninput={(event) => onInput(event)}
     />  
-  </div>       
+    {#if !currentInputValue || currentInputValue.trim().length === 0}
+      <Icon path={mdiPencilPlusOutline} size="20" class="hidden xs:block absolute left-3.5 top-4.5 text-gray-400 dark:text-gray-200" />
+    {/if}
+  </div>
   <div class="flex mt-2.5 text-gray-600 dark:text-gray-200">
     <button type="button" class="flex-1 border rounded-l-full bg-gray-50 dark:bg-gray-800 dark:border-gray-600 pl-3 pr-1 py-1 text-xs flex items-center gap-x-1 font-semibold truncate select-none hover:bg-gray-200 active:bg-gray-200 dark:active:bg-gray-900 transition-colors duration-300 ease-in-out" onclick={onHidePerson}>
       <Icon path={mdiEyeOffOutline} size="20" class="hidden xs:block flex-none" /> <p class="flex-1 text-center">{$t('hide_person')}</p>
