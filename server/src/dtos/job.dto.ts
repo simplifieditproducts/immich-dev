c9382e71e3d15d29e1a49bc171915466978be6a8
Hao Xi <haoxi911@gmail.com>
2025-12-23 11:32:27 -0500

    fix: queuing facial recognition jobs for a single user ends up with queuing jobs for all users.

--- server/src/dtos/job.dto.ts
+++ server/src/dtos/job.dto.ts
@@ -1,11 +1,10 @@
-import { ApiProperty } from '@nestjs/swagger';
 import { ManualJobName } from 'src/enum';
-import { ValidateEnum } from 'src/validation';
+import { ValidateEnum, ValidateUUID } from 'src/validation';
 
 export class JobCreateDto {
   @ValidateEnum({ enum: ManualJobName, name: 'ManualJobName' })
   name!: ManualJobName;
 
-  @ApiProperty({ required: false })
+  @ValidateUUID({ optional: true })
   ownerId?: string;
 }
